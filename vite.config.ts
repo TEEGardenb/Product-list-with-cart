83fb573d25f43fdce1eb0f3a7bde34929487a114
Mateo Parra <mateoparra1171@gmail.com>
2025-07-22 17:23:37 -0500

    gh-pages_config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'https://teegardenb.github.io/Product-list-with-cart/',
+  base: '/Product-list-with-cart/',
 })
